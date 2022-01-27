98f2a47adbaaa21d98944b36b1c20360c45b7028
BombCyclone <92756212+BombCyclone@users.noreply.github.com>
2022-01-27 15:42:30 -0600

    Initial Setup
    Hello World! Proof of Concept for Website

---
+++ tacos-website/src/test/java/com/lutheroaks/tacoswebsite/TacosWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lutheroaks.tacoswebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacosWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
