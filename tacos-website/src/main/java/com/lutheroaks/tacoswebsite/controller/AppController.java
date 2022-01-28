0161264250cf9f7e0a9e168d0f345f51a9a8bbbe
BombCyclone <92756212+BombCyclone@users.noreply.github.com>
2022-01-27 22:11:54 -0600

    Added Encouraging Meme
    Very Nice

--- tacos-website/src/main/java/com/lutheroaks/tacoswebsite/controller/AppController.java
+++ tacos-website/src/main/java/com/lutheroaks/tacoswebsite/controller/AppController.java
@@ -1,13 +1,13 @@
 package com.lutheroaks.tacoswebsite.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class AppController {
     
     @GetMapping("")
 	public String viewIndex() {
-		return "Hello World!";
+		return "../static/index.html";
 	}
 }
