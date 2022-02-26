3635fd00afd8e85be6600520102d384d53a70044
codeKat99 <91572800+codeKat99@users.noreply.github.com>
2022-02-26 16:57:27 -0600

    Added contact us page email functionality

--- src/main/java/com/lutheroaks/tacoswebsite/controller/PageRoutingController.java
+++ src/main/java/com/lutheroaks/tacoswebsite/controller/PageRoutingController.java
@@ -1,13 +1,21 @@
 package com.lutheroaks.tacoswebsite.controller;
 
 
+import javax.servlet.http.HttpServletRequest;
+
+import com.lutheroaks.tacoswebsite.SendEmailService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 // This class only contains logic necessary for page routing
 @Controller
 public class PageRoutingController {
-	
+
 	@GetMapping("/index")
 	public String viewIndex() {
 		return "index";
@@ -51,6 +59,27 @@ public class PageRoutingController {
 	@GetMapping("/login")
 	public String viewLogin() {
 		return "login";
+	}	
+
+
+	@Autowired
+	private JavaMailSender javaMailSender;
+	@PostMapping("/contact") //is called after someone inputs feedback
+	public String sendMail(HttpServletRequest request) {
+		String message = request.getParameter("message");
+		System.out.println(message);
+		String name = request.getParameter("name");
+		System.out.println(name);
+		SimpleMailMessage email = new SimpleMailMessage();
+        email.setFrom("tacosemailservice@gmail.com");
+        email.setTo("jdkata1@ilstu.edu");
+        email.setSubject("TACOS Contact Us Request from " + name);
+        email.setText(message);
+        javaMailSender.send(email);
+        System.out.println("Sent message successfully");
+		//javaMailSender.sendEmail("jdkata1@ilstu.edu", "This is the body", "Subject");
+		return "index";
 	}
+
 
 }
