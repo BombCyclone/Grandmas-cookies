f2f272ef6cd015f0e47f460f6da9ef4531864302
Pacelover <70454344+Pacelover@users.noreply.github.com>
2022-03-13 20:12:32 -0500

    Avoid duplicate member/resident creations

--- src/main/java/com/lutheroaks/tacoswebsite/member/MemberRepo.java
+++ src/main/java/com/lutheroaks/tacoswebsite/member/MemberRepo.java
@@ -1,8 +1,15 @@
 package com.lutheroaks.tacoswebsite.member;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MemberRepo extends JpaRepository<Member,Integer> {
+
+    //returns list of members by email
+    @Query(nativeQuery = true, value = "select * from Member where email = ?1")
+    List<Object> findMemberByEmail(String email);
     
 }
--- src/main/java/com/lutheroaks/tacoswebsite/resident/ResidentRepo.java
+++ src/main/java/com/lutheroaks/tacoswebsite/resident/ResidentRepo.java
@@ -1,8 +1,15 @@
 package com.lutheroaks.tacoswebsite.resident;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ResidentRepo extends JpaRepository<Resident,Integer> {
-    
+
+    //returns list of residents by first AND last name
+    @Query(nativeQuery = true, value = "select * from Resident where firstName = ?1 and lastName = ?1")
+    List<Object> findResidentByName(String firstName, String lastName);
+
 }
