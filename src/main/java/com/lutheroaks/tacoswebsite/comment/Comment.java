b9d411e024c5fd67977ce27b14664f15b3598fa4
Pacelover <70454344+Pacelover@users.noreply.github.com>
2022-03-17 19:41:35 -0500

    Add comment java classes for db implementation

---
+++ src/main/java/com/lutheroaks/tacoswebsite/comment/Comment.java
@@ -0,0 +1,44 @@
+package com.lutheroaks.tacoswebsite.comment;
+
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+
+import com.lutheroaks.tacoswebsite.member.Member;
+import com.lutheroaks.tacoswebsite.tickets.Tickets;
+
+import lombok.Data;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+// This class contains the attributes and getters/setters for the Comment table in the database
+@Entity
+@Data
+@Table(name = "Comment")
+@RequiredArgsConstructor
+public class Comment {
+
+    @Column(nullable = false, unique = false, length = 300)
+    @NonNull private String content;
+
+    @Column(nullable = false, unique = false, length = 300, columnDefinition="TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+    private Timestamp timeStamp;
+
+    @Id
+    @ManyToOne
+    @JoinColumn(name="ticket_num")
+    private Tickets ticketNum;
+
+    @Id
+    @ManyToOne
+    @JoinColumn(name="memberId")
+    private Member memberId;
+
+}
+
--- src/main/java/com/lutheroaks/tacoswebsite/controllers/database/CommentController.java
+++ src/main/java/com/lutheroaks/tacoswebsite/controllers/database/CommentController.java
@@ -1,5 +1,37 @@
 package com.lutheroaks.tacoswebsite.controllers.database;
 
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.lutheroaks.tacoswebsite.comment.Comment;
+import com.lutheroaks.tacoswebsite.comment.CommentRepo;
+
+@RestController
 public class CommentController {
+
+    private CommentRepo repository;
+
+    @Autowired
+    public CommentController(CommentRepo repository){
+        this.repository = repository;
+    }
+
+    // this method adds a new row to the comment table
+	@PostMapping("/comment")
+	public String addComment(String content) {
+			Comment toAdd = new Comment(content);
+			repository.save(toAdd);
+			return "A new comment was added!";
+	}
+
+	// this method returns a list of all rows in the member table
+	@GetMapping("/comment")
+	public List<Comment> getComments() {
+		return repository.findAll();
+	}
     
 }
+ 
