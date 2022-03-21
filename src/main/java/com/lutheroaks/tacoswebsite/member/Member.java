b2d11a1e5fc36d50d0e38803134cfa1d8977dcdb
BombCyclone <92756212+BombCyclone@users.noreply.github.com>
2022-03-20 22:09:39 -0500

    Temporarily lowering test coverage threshold

--- src/main/java/com/lutheroaks/tacoswebsite/member/Member.java
+++ src/main/java/com/lutheroaks/tacoswebsite/member/Member.java
@@ -17,7 +17,6 @@ import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
-import javax.validation.constraints.Size;
 
 import com.lutheroaks.tacoswebsite.bio.Bio;
 import com.lutheroaks.tacoswebsite.comment.Comment;
