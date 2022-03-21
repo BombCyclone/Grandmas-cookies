4f7b9fc8a6ac2d37d6757d007a8658b5a424884f
Nick Anderson <92756212+BombCyclone@users.noreply.github.com>
2022-03-20 22:40:56 -0500

    Attempting to resolve deployed login issue

--- src/main/java/com/lutheroaks/tacoswebsite/TacosWebsiteApplication.java
+++ src/main/java/com/lutheroaks/tacoswebsite/TacosWebsiteApplication.java
@@ -3,7 +3,7 @@ package com.lutheroaks.tacoswebsite;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class TacosWebsiteApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TacosWebsiteApplication.class, args);
