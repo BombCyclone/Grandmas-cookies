aee9278f8de7b99a22cf51b52bb91ed72c1d2c28
BombCyclone <92756212+BombCyclone@users.noreply.github.com>
2022-04-12 11:08:52 -0500

    KBService Added

--- src/main/java/com/lutheroaks/tacoswebsite/controllers/database/KbpostController.java
+++ src/main/java/com/lutheroaks/tacoswebsite/controllers/database/KbpostController.java
@@ -1,6 +1,5 @@
 package com.lutheroaks.tacoswebsite.controllers.database;
 
-import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -8,32 +7,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.IOException;
-import java.security.Principal;
-import java.sql.Timestamp;
-import java.time.Instant;
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 import com.lutheroaks.tacoswebsite.entities.kb.KBPost;
 import com.lutheroaks.tacoswebsite.entities.kb.KBPostRepo;
-import com.lutheroaks.tacoswebsite.entities.member.Member;
-import com.lutheroaks.tacoswebsite.entities.member.MemberRepo;
+import com.lutheroaks.tacoswebsite.entities.kb.KBService;
 
 @RestController
 public class KbpostController {
-
-    // for logging information to console
-    private Logger logger = org.slf4j.LoggerFactory.getLogger(KbpostController.class);
     
     @Autowired
     private KBPostRepo repository;
 
     @Autowired
-    private MemberRepo memberRepo;
+    private KBService service;
 
     /**
      * Adds a KB post to the table
@@ -42,27 +31,8 @@ public class KbpostController {
      * @throws IOException
      */
     @PostMapping("/kbpost")
-	public void addKBPost(final HttpServletRequest request, 
-                        final HttpServletResponse response) throws IOException {
-        KBPost toAdd = new KBPost();
-        try {
-            // get the email (user name) of the member member who is posting from the request details
-            Principal principal = request.getUserPrincipal();
-            String userName = principal.getName();
-            Member poster = memberRepo.findMemberByEmail(userName);
-            toAdd.setMember(poster);
-            // set the title and content of the kbpost based on the request
-            toAdd.setTitle(request.getParameter("title"));
-            toAdd.setContent(request.getParameter("content"));
-            toAdd.setTags(new ArrayList<>());
-            // use the current time as the timestamp
-            toAdd.setTimeStamp(Timestamp.from(Instant.now()));
-            repository.save(toAdd);
-            response.sendRedirect("index");
-        } catch (Exception e) {
-            logger.error("An error occurred while adding a KB Post: ", e);
-            response.sendRedirect("error");
-        }
+	public void addKBPost(final HttpServletRequest request) {
+        service.createPost(request);
 	}
     /**
      * Returns a list of all KB posts in the table
@@ -81,10 +51,8 @@ public class KbpostController {
      */
     @Transactional
     @DeleteMapping("/kbpost")
-    public void deleteKBPost(final HttpServletRequest request, final HttpServletResponse response) throws IOException {
-        int postId = Integer.parseInt(request.getParameter("postId"));
-        repository.deleteKBPostById(postId);
-        response.sendRedirect("kbpost");
+    public void deleteKBPost(final HttpServletRequest request) {
+        service.removePost(request);
     }
 
 }
