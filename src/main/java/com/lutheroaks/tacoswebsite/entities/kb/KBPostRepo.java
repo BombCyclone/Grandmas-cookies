ed7c1b511a3ff34b6183f99a54e7e1a78d5e244f
BombCyclone <92756212+BombCyclone@users.noreply.github.com>
2022-04-18 22:13:51 -0500

    Queries added for comment and kbpost lookup

--- src/main/java/com/lutheroaks/tacoswebsite/entities/kb/KBPostRepo.java
+++ src/main/java/com/lutheroaks/tacoswebsite/entities/kb/KBPostRepo.java
@@ -3,10 +3,14 @@ package com.lutheroaks.tacoswebsite.entities.kb;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface KBPostRepo extends JpaRepository<KBPost,Integer> {
+
+    @Query("SELECT x FROM KBPost x WHERE x.postId = :id")
+    KBPost findPostById(@Param("id") int id);
 
     @Modifying
     @Query(value = "DELETE FROM kbpost WHERE post_id = ?1", nativeQuery = true)
